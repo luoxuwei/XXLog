a6da9eb3a8aaf3e4c347b3411d8e94bf8767ab85
luoxuwei <xwluo@foxmail.com>
2021-11-10 04:31:08 +0800

    封装互斥锁相关操作

---
+++ xxlog/src/main/cpp/util/mutex.cc
@@ -0,0 +1,35 @@
+//
+// Created by 罗旭维 on 2021/11/10.
+//
+
+#include "mutex.h"
+
+namespace xxlog {
+    Mutex::Mutex() {
+        pthread_mutex_init(&mutex_, NULL);
+    }
+
+    Mutex::~Mutex() {
+        pthread_mutex_destroy(&mutex_);
+    }
+
+    pthread_mutex_t * Mutex::GetMutex() {
+        return &mutex_;
+    }
+
+    void Mutex::Lock() {
+        pthread_mutex_lock(&mutex_);
+    }
+
+    void Mutex::Unlock() {
+        pthread_mutex_unlock(&mutex_);
+    }
+
+    MutexGuard::MutexGuard(Mutex &_mutex) : mutex_(_mutex) {
+        mutex_.Lock();
+    }
+
+    MutexGuard::~MutexGuard() {
+        mutex_.Unlock();
+    }
+}
---
+++ xxlog/src/main/cpp/util/mutex.h
@@ -0,0 +1,35 @@
+//
+// Created by 罗旭维 on 2021/11/10.
+//
+
+#ifndef XXLOG_MUTEX_H
+#define XXLOG_MUTEX_H
+#include <pthread.h>
+
+namespace xxlog {
+    class Mutex {
+    public:
+        Mutex();
+        ~Mutex();
+
+        void Lock();
+        void Unlock();
+
+        pthread_mutex_t *GetMutex();
+    private:
+        pthread_mutex_t mutex_;
+    };
+
+    class MutexGuard {
+    public:
+        MutexGuard(Mutex &);
+        ~MutexGuard();
+
+    private:
+        Mutex &mutex_;
+    };
+}
+
+
+
+#endif //XXLOG_MUTEX_H
